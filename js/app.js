9cd03eb6c4b940e4bf71c0adb76a8ec5be4f2f2f
Dave Charnuska <davecharnuska@gmail.com>
2016-05-05 14:28:10 -0400

    add pagination

--- js/app.js
+++ js/app.js
@@ -1,13 +1,24 @@
-var feed = new Instafeed({
-        get: 'tagged',
-        tagName: 'bernie',
-        accessToken: '3036055374.1677ed0.22d0ddfa3d564b30983c325885530edc'
-    });
-feed.run();
-// $.ajax({
-// 	url: 'https://api.instagram.com/v1/tags/sun?access_token=3036055374.1677ed0.22d0ddfa3d564b30983c325885530edc',
-// 	dataType: 'jsonp',
-// 	success: function(response) {
-// 		console.log(response);
-// 	}
-// });
+$(document).ready(function(){
+	var loadButton = $("#load-more");
+	var feed = new Instafeed({
+	        get: 'tagged',
+	        tagName: 'mediabern',
+					resolution: 'low_resolution',
+					limit: 600,
+	        accessToken: '3036055374.1677ed0.22d0ddfa3d564b30983c325885530edc',
+					after: function() {
+		    			// disable button if no more results to load
+		    			if (!this.hasNext()) {
+		      			loadButton.setAttribute('disabled', 'disabled');
+		    			}
+		  		}
+	    });
+
+	// bind the load more button
+	loadButton.click(function() {
+	  feed.next();
+	});
+
+	feed.run();
+})
+
